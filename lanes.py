af478202d4846d524ba49043b8bdb29a4dd756ec
BatuhanUlucay <hulucay@etu.edu.tr>
2019-09-18 08:33:40 +0300

    masked the region of interest.

--- lanes.py
+++ lanes.py
@@ -1,10 +1,28 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('test_image.jpg') # cv2 helps us to read the image as a matrix
+#transforms the parameter image to gray (easy to process) and makes it an 5,5 gaussian blurred.
+def canny(image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5, 5), 0)
+    canny = cv2.Canny(blur,50, 150) # Canny function detects edges by taking derivatives on the x and y axis and decides whether a pixel is an edge or not.
+    return canny
+
+
+#masking the region of interest.
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200, height), (1100, height), (550, 250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+
+image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY) # we need to convert our image to RGB2GRAY because int this way, it is faster and more accurate to process the edges.
-blur = cv2.GaussianBlur(gray, (5,5), 0) # this is optional since, canny() function already makes a 5,5 Gaussian when we call it.
-canny = cv2.Canny(blur, 50, 150) # canny function detects edges by taking derivatives on the x and y axis and decides whether a pixel is an edge or not.
-cv2.imshow('result', canny)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+cv2.imshow('result', cropped_image)
 cv2.waitKey(0)
